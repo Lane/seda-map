1e5e71e3599f669591effe76d0cd869b37961251
Lane Olson <laneolson@gmail.com>
2019-02-12 14:11:56 -0800

    update range

--- src/modules/mapColors.js
+++ src/modules/mapColors.js
@@ -1,5 +1,5 @@
 const initialState = [
-  '#052458', '#1E69AD', '#48A5E8', '#D8F0F9', '#f7f7f7', '#e6f5d0', '#b8e186', '#7fbc41', '#4d9221'
+  '#052458', '#1E69AD', '#48A5E8', '#D8F0F9', '#e7f7f7', '#e6f5d0', '#b8e186', '#7fbc41', '#4d9221'
 ]
 
 const mapColors = (state = initialState, action) => {
